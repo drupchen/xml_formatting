79a5ba35a923ea83ec1e800c11a68b431515fa38
drupchen <hhdrupchen@gmail.com>
2016-11-30 16:35:59 +0100

    A formatting the body of the text

--- to_xml/xml_formatting.py
+++ to_xml/xml_formatting.py
@@ -25,4 +25,24 @@ for f in os.listdir('{}/{}'.format(in_path, in_folder)):
     parsed = []
     for page_num in range(0, len(pages)-1, 2):
         parsed.append((pages[page_num], pages[page_num+1].split('\n')))
+
+    formatted_pages = []
+    for p in parsed:
+        tif = p[0]
+        lines = p[1]
+        if lines == [''] or lines == ['No content captured']:
+            page = '<tei:p unit="page" n="{}/>'.format(tif)
+            formatted_pages.append(page)
+        else:
+            if len(lines) == 1:
+                page = '<tei:p unit="page" n="{}"/>{}'.format(tif, ''.join(lines))
+                formatted_pages.append(page)
+            else:
+                line_format = '<tei:milestone unit="line" n="{}"/>{}'
+                formatted_lines = [line_format.format(num+1, line) for num, line in enumerate(lines)]
+                page = '<tei:p unit="page" n="{}">{}</tei:p>'.format(tif, ''.join(formatted_lines))
+                formatted_pages.append(page)
+
+    formatted_text = '<tei:text><tei:body><tei:div>{}</tei:div></tei:body></tei:text>'.format(''.join(formatted_pages))
+
     print('ok')
