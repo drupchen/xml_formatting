1c0af624b9cdc097c6d45cf662867922aa0317e7
drupchen <hhdrupchen@gmail.com>
2016-11-30 22:20:06 +0100

    create function for writing data + structure

--- to_xml/xml_formatting.py
+++ to_xml/xml_formatting.py
@@ -54,22 +54,32 @@ def format_header(metadata):
     SRC_PATH = metadata['SRC_PATH'] # Namsel_OCR/Batch-20140903/W00EGS1016733/xml/W00EGS1016733-I01JW48/W00EGS1016733-I01JW48.xml
     return '<tei:teiHeader><tei:fileDesc><tei:titleStmt><tei:title>{}</tei:title></tei:titleStmt><tei:publicationStmt><tei:distributor>\n{}\n</tei:distributor><tei:idno type="TBRC_TEXT_RID">{}</tei:idno><tei:idno type="page_equals_image">page_equals_image</tei:idno></tei:publicationStmt><tei:sourceDesc><tei:bibl><tei:idno type="TBRC_RID">{}</tei:idno><tei:idno type="SRC_PATH">{}</tei:idno></tei:bibl></tei:sourceDesc></tei:fileDesc></tei:teiHeader>'.format(text_title, distributor_comment, TBRC_text_RID, TBRC_RID, SRC_PATH)
 
+
+def write_in_folder_structure(metadata, tei):
+    out_folder = 'output/{}'.format(metadata[f]['TBRC_text_RID'].split('-')[0])
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+    work_folder = '{}/{}'.format(out_folder, metadata[f]['TBRC_text_RID'].replace('-0000', ''))
+    if not os.path.exists(work_folder):
+        os.makedirs(work_folder)
+    write_file('{}/{}.xml'.format(work_folder, metadata[f]['TBRC_text_RID']), tei)
+
+
 in_path = './raw_data'
 in_folder = 'W24829-OCR'
 metadata = yaml.load(open_file('{}/{}/metadata/meta.txt'.format(in_path, in_folder)))
 for f in os.listdir('{}/{}'.format(in_path, in_folder)):
     if f.endswith('.txt'):
         work_name = f.replace('.txt', '')
+        # open the OCR'd file and parse its content
         content = open_file('{}/{}/{}'.format(in_path, in_folder, f)).replace('OCR text', '')
         parsed = parse_content(content)
+        # format the body of the text
         formatted_text = format_text(parsed)
+        # format the xml header
         metadata[f]['SRC_PATH'] = 'To Change. {}/{}'.format(in_folder, f)
         formatted_header = format_header(metadata[f])
+        # put the whole xml together
         tei = '<tei:TEI xmlns:tei="http://www.tei-c.org/ns/1.0">{}{}</tei:TEI>'.format(formatted_header, formatted_text)
-        out_folder = 'output/{}'.format(metadata[f]['TBRC_text_RID'].split('-')[0])
-        if not os.path.exists(out_folder):
-            os.makedirs(out_folder)
-        work_folder = '{}/{}'.format(out_folder, metadata[f]['TBRC_text_RID'].replace('-0000', ''))
-        if not os.path.exists(work_folder):
-            os.makedirs(work_folder)
-        write_file('{}/{}.xml'.format(work_folder, metadata[f]['TBRC_text_RID']), tei)
+        # create the folder structure and write the xml files
+        write_in_folder_structure(metadata, tei)
